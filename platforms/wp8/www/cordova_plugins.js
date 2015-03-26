5c9877204a9a129b97ae3ccd3fb898ccb73ecbe7
Moataz Hammous <moataz.abdallh@gmail.com>
2015-03-27 00:08:24 +0200

    WP8

---
+++ platforms/wp8/www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/nl.x-services.plugins.toast/www/Toast.js",
+        "id": "nl.x-services.plugins.toast.Toast",
+        "clobbers": [
+            "window.plugins.toast"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.toast/test/tests.js",
+        "id": "nl.x-services.plugins.toast.tests"
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "nl.x-services.plugins.toast": "2.0.2",
+    "org.apache.cordova.dialogs": "0.3.0"
+}
+// BOTTOM OF METADATA
+});
